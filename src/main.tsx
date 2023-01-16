2a157e32e793c8c0212b95f39f4c11a1a8de9788
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-01-17 06:03:29 +0800

    Implement react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
