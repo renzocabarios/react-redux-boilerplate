06621e7216556b28ae83efb22d72f5bf26a825e8
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-01-17 06:13:27 +0800

    Implement redux toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Routes } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
+import { store } from "./states/store";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes></Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes></Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
