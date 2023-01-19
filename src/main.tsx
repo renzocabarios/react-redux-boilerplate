c2f9b0a20167d74344bfd95213e10099e101c3b1
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-01-19 15:07:08 +0800

    Add sample view

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./states/store";
 import { Provider } from "react-redux";
+import { Sample } from "./views";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <Routes></Routes>
+        <Routes>
+          <Route path="/" element={<Sample />} />
+        </Routes>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
---
+++ src/views/Sample/index.tsx
@@ -0,0 +1,5 @@
+function Sample() {
+  return <></>;
+}
+
+export default Sample;
